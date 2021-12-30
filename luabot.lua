ce3ea1c1c8698347f6c61ba7e2b5ec9817479a02
buu <69942450+xbuu@users.noreply.github.com>
2021-12-30 14:59:55 -0500

    Update luabot.lua

--- luabot.lua
+++ luabot.lua
@@ -4,11 +4,6 @@
  █▄▄ █▄█ █▀█ █▄█ █▄█ ░█░ v1.00
 ]]
 
-cmds = {
-    "botname",
-    "botprefix"
-}
-
 -- variables
 
 local players = game:GetService("Players")
@@ -28,16 +23,40 @@ Process: executed.
 
 chat("Welcome to Semi-Advanced [LuaBot] ".._G.version)
 
+cmds = {
+    "botname",
+    "botprefix",
+    "bring",
+    "kill",
+}
+
+sidecmds = {
+    "load"
+}
+
 for i,v in pairs(players:GetChildren()) do
     v.Chatted:Connect(function(msg)
+        local split = msg:split(" ")
         if msg:lower() == _G.prefix..cmds[1] then
             chat("Hello, I am ".._G.user)
         elseif msg:lower() == cmds[2] then
             chat("Current prefix is ".._G.prefix)
         elseif msg:lower() == "%k. ".._G.user then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/xbuu/chatbot_lua/main/kekw.lua"))()
-        elseif msg:lower() == "%w. ".._G.user then
-            chat("[LuaBot]: Warn(there is an issue with your message remote.)")
+        elseif msg:lower() == _G.prefix..cmds[3] then
+            localplr.Character.HumanoidRootPart.CFrame = workspace[v.Name].HumanoidRootPart.CFrame
+        elseif msg:lower() == _G.prefix..cmds[4] then
+            localplr.Character.Humanoid.Health = 0
+
+
+
+        --/ Side Commands -- Loading Commands (####################################################################################) --\
+        
+        elseif msg:lower() == _G.prefix..sidecmds[1].." brew" then
+            warn("LuaBot: hi")
+        elseif split[1] == _G.prefix.."%c" then
+            local yes = split[2]
+            chat(yes)
         end
     end)
 end
@@ -46,14 +65,27 @@ players.PlayerAdded:Connect(function(v)
     warn(_G.user..": Player ("..v.Name..") joined the server.")
     chat(_G.user..": Player ("..v.Name..") joined the server.")
     v.Chatted:Connect(function(msg)
+        local split = msg:split(" ")
         if msg:lower() == _G.prefix..cmds[1] then
             chat("Hello, I am ".._G.user)
         elseif msg:lower() == cmds[2] then
             chat("Current prefix is ".._G.prefix)
         elseif msg:lower() == "%k. ".._G.user then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/xbuu/chatbot_lua/main/kekw.lua"))()
-        elseif msg:lower() == "%w. ".._G.user then
-            chat("[LuaBot]: Warn(there is an issue with your message remote.)")
+        elseif msg:lower() == _G.prefix..cmds[3] then
+            localplr.Character.HumanoidRootPart.CFrame = workspace[v.Name].HumanoidRootPart.CFrame
+        elseif msg:lower() == _G.prefix..cmds[4] then
+            localplr.Character.Humanoid.Health = 0
+
+
+
+        --/ Side Commands -- Loading Commands (####################################################################################) --\
+        
+        elseif msg:lower() == _G.prefix..sidecmds[1].." brew" then
+            warn("LuaBot: hi")
+        elseif split[1] == _G.prefix.."%c" then
+            local yes = split[2]
+            chat(yes)
         end
     end)
 end)
